014b1e9cddf5cbe382a4a8069525f86b550ed0bb
Flavio Akira <fla_akira@hotmail.com>
2021-04-08 20:52:58 -0600

    s

---
+++ Data Modeling - visualization and assist decision/SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿select datepart(year,dt) as Year,country, avg(averagetemperature) as AVG​
+from GlobalLandTemperatures_GlobalLandTemperaturesByCountry ​
+where Country in ( 'Canada', 'France', 'Germany', 'Italy', 'Japan', 'United States', 'United Kingdom') ​
+and AverageTemperature like '%_%' and ​
+dt between '1900-01-01' and '2013-09-01'​
+group by datepart(year, dt), Country​
+order by country,datepart(year, dt)
+
+
+
+select datepart(year,dt) as Year, avg(LandAverageTemperature) as AVG ​
+from GlobalLandTemperatures_GlobalTemperatures​
+where LandAverageTemperature like '%_%' and ​
+dt between '1900-01-01' and '2013-09-01' ​
+group by datepart(year, dt)​
+order by datepart(year, dt)
